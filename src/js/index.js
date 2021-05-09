168933d19ad93ef986660e4805bb1cdec2f45e28
Ethan Alfaro <ctabbx@gmail.com>
2021-05-09 15:24:54 +0200

    Estructura e instalacion

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
-import '../index.html'
-import './main.js'
-import '../img/book.png'
-import '../img/iceland.png'
+import "../index.html";
+import "../../assets/main.scss";
+import "./main.js";
+import "../img/book.png";
+import "../img/iceland.png";
