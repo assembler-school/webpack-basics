472436f3ab4329004dd87a6fcb77f160d1c57151
Ethan Alfaro <ctabbx@gmail.com>
2021-05-09 13:45:38 +0200

    Reinstall

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import '../index.html'
-import './main.js'
-import '../img/book.png'
-import '../img/iceland.png'
+// import '../index.html'
+// import './main.js'
+// import '../img/book.png'
+// import '../img/iceland.png'
