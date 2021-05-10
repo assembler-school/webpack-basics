c84c3cd5d01ea1770aa75dcda1d75e4b916fc2ff
Brahim <brahimcasas@hotmail.es>
2021-05-10 11:59:29 +0200

    babel transpilation

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,17 @@ module.exports = {
         },
       },
 
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+
       // {
       //   test: /\.(png|jpg|svg)$/i,
       //   use: [
