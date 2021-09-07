2f7e3c1fc7879c72bc7321b1fa3411fd5c156781
Pere Serra Bosch <pere.serra.bosch@gmail.com>
2021-09-07 16:34:37 +0200

    new files architecture

--- webpack.config.js
+++ webpack.config.js
@@ -3,46 +3,30 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpack = require("webpack");
 
-
-
 module.exports = {
-
     mode: "development",
     devServer: {
-      historyApiFallback: true,
-      port: 4000,
-      open: true,
-      compress: true,
-      hot: true
+        historyApiFallback: true,
+        port: 4000,
+        open: true,
+        compress: true,
+        hot: true
     },
     entry: {
-
-        main: path.resolve(__dirname, "./src/index.js")
-
+        main: path.resolve(__dirname, "./src/main.js")
     },
-
     output: {
-
         path: path.resolve(__dirname, "./dist"),
-
         filename: "[name].bundle.js"
-
     },
-
     plugins: [
-
         new HtmlWebpackPlugin({
-
             title: "Webpack Workshop Test",
-
             template: path.resolve(__dirname, "./src/index.html"),
-
             filename: "index.html"
-
         }),
         new webpack.HotModuleReplacementPlugin(),
     ],
-
     module: {
         rules: [
             {
@@ -51,8 +35,8 @@ module.exports = {
                 use: ['babel-loader']
             },
             {
-              test: /\.(scss|css)$/,
-              use: ["style-loader", "css-loader", "sass-loader"]
+                test: /\.(scss|css)$/,
+                use: ["style-loader", "css-loader", "sass-loader"]
             }
         ]
     }
