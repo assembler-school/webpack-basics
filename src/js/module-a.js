7f7a3d884ccd5a7ccc4948e8de16b7757b08fc84
AntonioCopete <antcopjim@gmail.com>
2021-09-07 18:00:36 +0200

    added template string for color swap

--- src/js/module-a.js
+++ src/js/module-a.js
@@ -1,7 +1,23 @@
 let h1 = document.getElementById("calufa");
 
 const arrowF = () => {
+  let textColor = null;
   h1.classList.toggle("clase");
+
+  console.log(h1.style.fontSize);
+
+  if (h1.children.length < 1) {
+    let newElement = document.createElement("p");
+    h1.append(newElement);
+  }
+
+  let p = document.querySelector("#calufa > p");
+  if (h1.classList.contains("clase")) {
+    textColor = "Red";
+  } else {
+    textColor = "Black";
+  }
+  p.textContent = `The text color is: ${textColor}`;
 };
 
 export { h1, arrowF };
