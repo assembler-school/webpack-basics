96a23ab6df5a0bf209be1f2a1e215a5c54a43b3b
Pere Serra Bosch <pere.serra.bosch@gmail.com>
2021-09-07 16:41:38 +0200

    added build and dev scripts

--- webpack.config.js
+++ webpack.config.js
@@ -3,41 +3,46 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpack = require("webpack");
 
-module.exports = {
-    mode: "development",
-    devServer: {
-        historyApiFallback: true,
-        port: 4000,
-        open: true,
-        compress: true,
-        hot: true
-    },
-    entry: {
-        main: path.resolve(__dirname, "./src/main.js")
-    },
-    output: {
-        path: path.resolve(__dirname, "./dist"),
-        filename: "[name].bundle.js"
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: "Webpack Workshop Test",
-            template: path.resolve(__dirname, "./src/index.html"),
-            filename: "index.html"
-        }),
-        new webpack.HotModuleReplacementPlugin(),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
-            },
-            {
-                test: /\.(scss|css)$/,
-                use: ["style-loader", "css-loader", "sass-loader"]
-            }
-        ]
+module.exports = (env, { mode }) => {
+    console.log({ mode })
+
+    const isProduction = mode === 'production'
+
+    return {
+        devServer: {
+            historyApiFallback: true,
+            port: 4000,
+            open: true,
+            compress: true,
+            hot: true
+        },
+        entry: {
+            main: path.resolve(__dirname, "./src/main.js")
+        },
+        output: {
+            path: path.resolve(__dirname, "./build"),
+            filename: isProduction ? '[name].[contenthash].js' : '[name].bundle.js',
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: "Webpack Workshop Test",
+                template: path.resolve(__dirname, "./src/index.html"),
+                filename: "index.html"
+            }),
+            new webpack.HotModuleReplacementPlugin(),
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: ['babel-loader']
+                },
+                {
+                    test: /\.(scss|css)$/,
+                    use: ["style-loader", "css-loader", "sass-loader"]
+                }
+            ]
+        }
     }
 }
