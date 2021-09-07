899dad8da88efdbaf07f9ec2054caff8baca0d3b
sanadriu <skualed@gmail.com>
2021-09-07 16:40:40 +0200

    Save changes

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+	mode: "development",
+	entry: {
+		main: path.resolve(__dirname, "./src/index.js"),
+	},
+	output: {
+		path: path.resolve(__dirname, "./dist"),
+		filename: "[name].js",
+	},
+	plugins: [
+		new webpack.ProvidePlugin({
+			$: "jquery",
+			jQuery: "jquery",
+		}),
+		new HtmlWebpackPlugin({
+			title: "Webpack Basics",
+			template: path.resolve(__dirname, "./src/index.html"),
+			filename: "index.html",
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: ["babel-loader"],
+			},
+			{
+				test: /\.(scss|sass)$/,
+				use: ["style-loader", "css-loader", "sass-loader"],
+			},
+		],
+	},
+};
