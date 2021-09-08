475897c09ed12fde0919b96ae2d201b9dd02646f
Pere Serra Bosch <pere.serra.bosch@gmail.com>
2021-09-08 11:41:59 +0200

    clean webpack previous files

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require("webpack");
 
 module.exports = (env, { mode }) => {
@@ -39,7 +40,19 @@ module.exports = (env, { mode }) => {
             new webpack.ProvidePlugin({
                 $: "jquery",
                 jQuery: "jquery"
-            })
+            }),
+            /**
+             * All files inside webpack's output.path directory will be removed once, but the
+             * directory itself will not be. If using webpack 4+'s default configuration,
+             * everything under <PROJECT_DIR>/dist/ will be removed.
+             * Use cleanOnceBeforeBuildPatterns to override this behavior.
+             *
+             * During rebuilds, all webpack assets that are not used anymore
+             * will be removed automatically.
+             *
+             * See `Options and Defaults` for information
+             */
+            new CleanWebpackPlugin(),
         ],
         module: {
             rules: [
