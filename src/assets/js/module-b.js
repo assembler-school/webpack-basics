fd1bad817e793b5d9bd0b81f529835c65bf31ec7
Pere Serra Bosch <pere.serra.bosch@gmail.com>
2021-09-08 10:27:01 +0200

    added scss

--- src/assets/js/module-b.js
+++ src/assets/js/module-b.js
@@ -1,5 +1,5 @@
 /* A file called module-b.js that contains a javascript module that makes use of the JQuery library (JQuery must be imported via webpack not manually)
  */
-export function jq() {
+export const jq = () => {
   $('body').append('<h1>Hello Webpacker!</h1>');
 }
