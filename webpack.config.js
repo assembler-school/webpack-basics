51954f41578e33a0a853544fc36424a78a6c04cd
Haroon Ahmad <haroon334@gmail.com>
2021-09-08 17:01:30 +0200

    Compressed Images

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,35 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              mozjpeg: {
+                progressive: true,
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.9],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75,
+              },
+            },
+          },
+        ],
+      },
+      {
         test: /\.s[ac]ss$/i,
 
         use: [
@@ -20,19 +49,6 @@ module.exports = {
           "css-loader",
           // Compiles Sass to CSS
           "sass-loader",
-        ],
-      },
-      {
-        test: /\.(png|jp(e*)g|svg)$/,
-
-        use: [
-          {
-            loader: "url-loader",
-            options: {
-              limit: 8000,
-              name: "./src/img/[hash]-name.[ext]",
-            },
-          },
         ],
       },
     ],
